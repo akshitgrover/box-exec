77905f5373c98a3267c54d6bfc33efb659df5249
akshit grover <akshit.grover2016@gmail.com>
2018-04-30 23:43:33 +0530

    Add Stage Two | Three

--- stage.js
+++ stage.js
@@ -27,6 +27,43 @@ const one = (image,lang)=>{
 	});
 }
 
+//Stage Two : Copy Source Code File In The Container
+
+const two = (lang,codefile)=>{
+	const container_name = "box-exec-" + lang;
+	return new Promise((resolve,reject)=>{
+		child.exec("docker cp " + codefile + " " + container_name + ":/",(error,stdout,stderr)=>{
+			if(error || stderr){
+				reject();
+			}
+			resolve();
+		});
+	});
+}
+
+//Stage Three: Compile Source Code File (only for c/c++)
+
+const three = (lang,codefile)=>{
+	const container_name = "box-exec-" + lang;
+	codefile = codefile.replace(/\\/g,"/");
+	codefile = codefile.split("/");	
+	const filename = codefile[codefile.length - 1];
+	const raw_name = filename.slice(0,filename.indexOf(".")) + ".out";
+	return new Promise((resolve,reject)=>{
+		child.exec("docker container exec " + container_name + " g++ -o " + raw_name + " " + filename,(error,stdout,stderr)=>{
+			if(error){
+				reject(error);
+			}
+			if(stderr){
+				reject(stderr);
+			}
+			resolve(stdout);
+		});
+	});
+}
+
 module.exports = {
-	one
+	one,
+	two,
+	three
 }
