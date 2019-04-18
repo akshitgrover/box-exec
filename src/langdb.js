36d45339b5ca712497c6221e6adabd8b93b48023
Akshit Grover <akshit.grover2016@gmail.com>
2019-04-18 13:42:53 +0530

    [skip travis] Add language db

---
+++ src/langdb.js
@@ -0,0 +1,22 @@
+module.exports = {
+  c: {
+    inputFile: 'code.c',
+    outputFile: 'a.out',
+  },
+  cpp: {
+    inputFile: 'code.cpp',
+    outputFile: 'a.out',
+  },
+  python2: {
+    inputFile: 'code.py',
+    outputFile: 'code.py',
+  },
+  python3: {
+    inputFile: 'code.py',
+    outputFile: 'code.py',
+  },
+  java8: {
+    inputFile: 'main.java',
+    outputFile: 'main',
+  },
+};
