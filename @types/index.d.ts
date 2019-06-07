67b4939d75a5bd93d58789832bd1eef668d7164e
Akshit Grover <akshit.grover2016@gmail.com>
2019-06-07 15:04:11 +0530

    Types: Add TestCase interface

--- @types/index.d.ts
+++ @types/index.d.ts
@@ -5,8 +5,18 @@ import * as Emitter from 'events';
 /*
   Structure of [BoxExec Instance].output
 */
-interface output {
+interface Output {
   readonly error: bool;
   readonly timeout: bool;
   readonly output: string;
+}
+
+/*
+  TestCase object structure.
+
+  Passed in [BoxExec Instance].setData();
+*/
+interface TestCase {
+  file: string;
+  timeout: number;
 }
