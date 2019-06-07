35f276c12b9da3fa67b8821f45a89921c5455e2f
Akshit Grover <akshit.grover2016@gmail.com>
2019-06-08 01:31:05 +0530

    Types: Move type declarations to namespace

--- @types/index.d.ts
+++ @types/index.d.ts
@@ -3,54 +3,56 @@
 import * as Emitter from 'events';
 
 /*
-  Structure of [BoxExec Instance].output
-*/
-interface Output {
-  readonly error: boolean;
-  readonly timeout: boolean;
-  readonly output: string;
-}
-
-/*
-  TestCase object structure.
-
-  Passed in [BoxExec Instance].setData();
-*/
-interface TestCase {
-  file: string;
-  timeout: number;
-}
-
-/*
-  BoxExec emitter class
-
-  User facing interface
-*/
-declare class ExecEmitter extends Emitter{
-
-  // Readonly instance variables
-  readonly error: boolean;
-  readonly errortext: Error | null;
-  readonly output: Output | null;
-
-  // Configurable variables
-  private language: string | null;
-  private codefile: string | null;
-  private testcasefiles: string | null;
-
-  // Configurartion member functions
-  setData(x: number, y: string, z: TestCase[]): void;
-  execute(): void;
-}
-
-/*
   ExecEmitter constructor function
 */
-declare function getEmitter(): ExecEmitter;
+declare function getEmitter(): getEmitter.ExecEmitter;
 
 /*
   Proxy namespace for ES6 imports
 */
-declare namespace getEmitter { }
+declare namespace getEmitter {
+
+  /*
+    BoxExec emitter class
+
+    User facing interface
+  */
+  class ExecEmitter extends Emitter{
+
+    // Readonly instance variables
+    readonly error: boolean;
+    readonly errortext: Error | null;
+    readonly output: Output | null;
+
+    // Configurable variables
+    private language: string | null;
+    private codefile: string | null;
+    private testcasefiles: string | null;
+
+    // Configurartion member functions
+    setData(x: number, y: string, z: TestCase[]): void;
+    execute(): void;
+  }
+
+  /*
+    Structure of [BoxExec Instance].output
+  */
+  interface Output {
+    readonly error: boolean;
+    readonly timeout: boolean;
+    readonly output: string;
+  }
+
+  /*
+    TestCase object structure.
+
+    Passed in [BoxExec Instance].setData();
+  */
+  interface TestCase {
+    file: string;
+    timeout: number;
+  }
+
+}
 
 export = getEmitter;
