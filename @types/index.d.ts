c216a0dadc56c2fad7df296e769d95363d8faabf
Akshit Grover <akshit.grover2016@gmail.com>
2019-06-07 15:38:40 +0530

    [skip travis] Types: Declare ExecEmitter class

--- @types/index.d.ts
+++ @types/index.d.ts
@@ -20,3 +20,29 @@ interface TestCase {
   file: string;
   timeout: number;
 }
+
+/*
+  BoxExec emitter class
+
+  User facing interface
+*/
+declare class ExecEmitter extends Emitter{
+
+  // Readonly instance variables
+  readonly error: bool;
+  readonly errortext: Error | null;
+  readonly output: output | null;
+
+  // Configurable variables
+  private language: string | null;
+  private codefile: string | null;
+  private testcasefiles: string | null;
+
+  // Configurartion member functions
+  setData(x: number, y: string, z: TestCase[]): void;
+  execute(): void;
+}
+
+declare function getEmitter(): ExecEmitter;
+
+export = getEmitter;
