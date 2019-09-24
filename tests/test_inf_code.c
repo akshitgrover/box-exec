6713eb223ddb79fafd0529836b7c93f55586a1a0
Akshit Grover <akshit.grover2016@gmail.com>
2019-09-24 21:55:14 +0530

    Tests: Add infnite loop test code

---
+++ tests/test_inf_code.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <time.h>
+
+// void delay(int milliseconds)
+// {
+//     long pause;
+//     clock_t now,then;
+
+//     pause = milliseconds*(CLOCKS_PER_SEC/1000);
+//     now = then = clock();
+//     while( (now-then) < pause )
+//         now = clock();
+// }
+
+int main() {
+    // delay(25000);
+    while(1) { };
+    return 0;
+}
